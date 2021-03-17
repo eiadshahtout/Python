b443892bac7ecb5f99e775d2a10a133ac3aa663b
eiadshahtout <eiadshahtout@homtail.co.uk>
2021-03-17 23:29:18 +0400

    Changed list.py

--- list.py
+++ list.py
@@ -1,5 +1,5 @@
 # Define a set named sample_set.
-sample_set = {1.98, 98.9, 74.95, 2.5, 1, 16.3} 
+sample_set = {1.97, 98.9, 74.95, 2.5, 1, 16.3} 
 # Show the whole set
 print(sample_set)
 # Make a copy and shoe the copy.
