b97fdf887dde6fd5209ebe2323bf3fa32705d433
eiadshahtout <eiadshahtout@homtail.co.uk>
2021-03-21 00:09:58 +0400

    Added fruit.py

---
+++ fruit.py
@@ -0,0 +1,15 @@
+favouriteFruits = ["Bananas","Apples","Mangoes"]
+
+if "Bananas" in favouriteFruits:
+    print("You like bananas")
+else:
+    print("You don't like bananas")
+if "Peacjes" in favouriteFruits:
+    print("You like peaches")
+else:
+    print("You don't like peaches")
+if "Apples"in favouriteFruits:
+    print("You really like apples")
+else:
+    print("You don't like apples")
+
