8984176fc1052f05f41e77473f457a4acaefd40f
eiadshahtout <eiadshahtout@homtail.co.uk>
2021-04-25 03:49:18 +0400

    Added weather api which takes London info and returns numbers!

--- json/requestweather.py
+++ json/requestweather.py
@@ -1,18 +1,18 @@
 import requests
 
-url = "https://api.weatherapi.com/v1/current.json?key=d14bb1b4b449419aa0a214517211804&q=London&aqi=no"
+url = "https://api.weatherapi.com/v1/current.json?key=d14bb1b4b449419aa0a214517211804&q=Dubai&aqi=no"
 r = requests.get(url)
 print(f"Status code {r.status_code}")
 
 response_dicts = r.json()
 print(response_dicts)
-print("-" * 50)
+print("--" * 50)
 print(f"Full_info: {response_dicts['location']}")
 
-print("-" * 50)
+print("--" * 50)
 new_response = response_dicts['location']
 print(f"Name: {new_response['name']}")
 
 current_time = response_dicts['current']
-print("-" * 50)
+print("--" * 50)
 print(f"Current Time: {current_time['last_updated']}")
